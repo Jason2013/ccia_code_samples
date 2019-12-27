cd3f646064dcbbb7be29c968e2d5d18afa5a33f0
Chang Chen <Chang.Chen@amd.com>
2019-12-27 16:20:15 +0800

    Fix `listing_4.7.cpp`.

--- listings/listing_4.7.cpp
+++ listings/listing_4.7.cpp
@@ -3,8 +3,8 @@
 
 struct X
 {
-    void foo(int,std::string const&);
-    std::string bar(std::string const&);
+    void foo(int, std::string const&) {}
+    std::string bar(std::string const& s) { return s; }
 };
 
 
@@ -14,21 +14,26 @@ auto f2=std::async(&X::bar,x,"goodbye");
 
 struct Y
 {
-    double operator()(double);
+    double operator()(double) { return 0.0; }
 };
 Y y;
 auto f3=std::async(Y(),3.141);
 auto f4=std::async(std::ref(y),2.718);
-X baz(X&);
+X baz(X& x) { return x; }
 auto f6=std::async(baz,std::ref(x));
 class move_only
 {
 public:
-    move_only();
-    move_only(move_only&&);
-    move_only(move_only const&) = delete;
-    move_only& operator=(move_only&&);
+    move_only() {};
+    move_only(move_only&&) {}
+    move_only(move_only const&) {}
+    move_only& operator=(move_only&&) = delete;
     move_only& operator=(move_only const&) = delete;
-    void operator()();
+    void operator()() {};
 };
 auto f5=std::async(move_only());
+
+int main()
+{
+    return 0;
+}
